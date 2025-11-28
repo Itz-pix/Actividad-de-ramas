f3f5f20b9de3f46efc7607c11975c8c487f2c539
silxluv <silviayuuki@outlook.com>
2025-11-27 18:39:54 -0600

    Problema 3

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    double x, y, z;
+    cin>>x;
+    cin>>y;
+    cin>>z;
+    //Alguien saquenme de mi casita
+    double ev = 7+(2*(x)+(y)-(z)*x*x+y-z)/2*(x)+y+z;
+    cout<<ev;
+
+    return 0;
+}
